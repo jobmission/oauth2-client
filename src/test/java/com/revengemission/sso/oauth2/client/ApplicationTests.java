4c3df55072c359f9431b1a2b629ae43c0ac93596
wzhang <contact@revengemission.com>
2018-08-25 16:51:57 +0800

    update

--- src/test/java/com/revengemission/sso/oauth2/client/ApplicationTests.java
+++ src/test/java/com/revengemission/sso/oauth2/client/ApplicationTests.java
@@ -2,6 +2,7 @@ package com.revengemission.sso.oauth2.client;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
+    @Value("${host.api}")
+    private String apiHost;
+
     @Test
     public void contextLoads() {
+
+        //用登陆后的token ,请求api资源
+        //header格式，Authorization : Bearer xxxxx
+
+
     }
 
 }
