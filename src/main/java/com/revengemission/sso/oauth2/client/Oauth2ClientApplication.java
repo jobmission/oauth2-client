f30eb4a70395894d448e32df15c06b78c19b3872
jobmission <contact@revengemission.com>
2019-06-11 20:05:56 +0800

    update

--- src/main/java/com/revengemission/sso/oauth2/client/Oauth2ClientApplication.java
+++ src/main/java/com/revengemission/sso/oauth2/client/Oauth2ClientApplication.java
@@ -3,13 +3,9 @@ package com.revengemission.sso.oauth2.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-*http://www.baeldung.com/sso-spring-security-oauth2
-*
-*https://github.com/eugenp/tutorials/tree/master/spring-security-sso
-*
-* https://github.com/spring-guides/tut-spring-boot-oauth2/blob/master/manual/src/main/java/com/example/SocialApplication.java
-* */
+/**
+ * https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#oauth2client
+ */
 @SpringBootApplication
 public class Oauth2ClientApplication {
 
--- src/main/java/com/revengemission/sso/oauth2/client/config/SecurityConfig.java
+++ src/main/java/com/revengemission/sso/oauth2/client/config/SecurityConfig.java
@@ -8,10 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 @Configuration
@@ -47,29 +45,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().mvcMatchers("/assets/**", "/img/**", "/favicon.ico");
     }
 
-    /*
+    /**
      * UserInfo Endpoint
      * Mapping User Authorities
-     *
+     * <p>
      * https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#oauth2login-advanced-map-authorities
-     * */
+     */
     private GrantedAuthoritiesMapper userAuthoritiesMapper() {
         return (authorities) -> {
             Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
             authorities.forEach(authority -> {
-                OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
+//                OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
+//                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
 
-                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
-
-                // Map the attributes found in userAttributes
-                // to one or more GrantedAuthority's and add it to mappedAuthorities
-                if (userAttributes.containsKey("authorities")) {
-                    String authoritiesStr = userAttributes.get("authorities").toString();
-                    String[] roles = authoritiesStr.split(",");
-                    for (String role : roles) {
-                        mappedAuthorities.add(new SimpleGrantedAuthority(role));
-                    }
+                String authoritiesStr = authority.getAuthority();
+                String[] roles = authoritiesStr.split(",");
+                for (String role : roles) {
+                    mappedAuthorities.add(new SimpleGrantedAuthority(role));
                 }
 
             });
