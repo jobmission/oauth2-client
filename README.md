4f55e80f01e24a12617ee2a0c82a88fc6b4464fe
wzhang <contact@revengemission.com>
2019-12-14 00:31:56 +0800

    upgrade

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## 基于spring boot2 sso Oauth2 Client
+## 基于spring boot2 sso Oauth2 Client JWT
 ## 启动前修改application.properties中的相关参数
 ### 当client和server在一台主机时，请用域名访问，否则cookies会相互覆盖，影响测试，以下配置仅供参考
 * hosts文件
--- src/main/java/com/revengemission/sso/oauth2/client/config/SecurityConfig.java
+++ src/main/java/com/revengemission/sso/oauth2/client/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.revengemission.sso.oauth2.client.config;
 
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.nimbusds.jwt.SignedJWT;
+import net.minidev.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +14,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.util.StringUtils;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    private Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
@@ -34,9 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .oauth2Login()
             .successHandler(customAuthenticationSuccessHandler)
-            .userInfoEndpoint()
-            .userAuthoritiesMapper(this.userAuthoritiesMapper());
-
+            .userInfoEndpoint().userService(oauth2UserService());
 
     }
 
@@ -46,28 +59,98 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
-     * UserInfo Endpoint
+     * 从user-info-uri 返回结果中抽取权限信息，如角色等，默认为scope
      * Mapping User Authorities
-     * access_token 中抽取权限信息，如角色等，默认为scope
      * https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#oauth2login-advanced-map-authorities
      */
+    @Deprecated
     private GrantedAuthoritiesMapper userAuthoritiesMapper() {
         return (authorities) -> {
             Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
             authorities.forEach(authority -> {
-///                OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
-///                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+                if (OidcUserAuthority.class.isInstance(authority)) {
+                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
 
-                String authoritiesStr = authority.getAuthority();
-                String[] roles = authoritiesStr.split(",");
-                for (String role : roles) {
-                    mappedAuthorities.add(new SimpleGrantedAuthority(role));
+                    System.out.println(oidcUserAuthority);
+
+                    // Map the claims found in idToken and/or userInfo
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                } else if (OAuth2UserAuthority.class.isInstance(authority)) {
+                    OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
+
+                    Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+                    System.out.println(userAttributes);
+                    // Map the attributes found in userAttributes
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                } else if (SimpleGrantedAuthority.class.isInstance(authority)) {
+                    SimpleGrantedAuthority simpleGrantedAuthority = (SimpleGrantedAuthority) authority;
+
+                    System.out.println(simpleGrantedAuthority);
+
                 }
-
             });
 
             return mappedAuthorities;
+        };
+    }
+
+    com.jayway.jsonpath.Configuration conf = com.jayway.jsonpath.Configuration.builder().options(Option.SUPPRESS_EXCEPTIONS).build();
+
+    /**
+     * 从access_token中直接抽取角色等信息
+     * https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#oauth2login-advanced-map-authorities-oauth2userservice
+     *
+     * @return
+     */
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+
+        return (userRequest) -> {
+            String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+            if (!StringUtils.hasText(userNameAttributeName)) {
+                userNameAttributeName = "sub";
+            }
+            OAuth2AccessToken accessToken = userRequest.getAccessToken();
+            Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+            try {
+                SignedJWT jwt = SignedJWT.parse(accessToken.getTokenValue());
+                String claimJsonString = jwt.getJWTClaimsSet().toJSONObject().toJSONString();
+                Object document = com.jayway.jsonpath.Configuration.defaultConfiguration().jsonProvider().parse(claimJsonString);
+
+                List<Object> authorities = JsonPath.using(conf).parse(document).read("$..roles");
+
+                if (authorities == null || authorities.size() == 0) {
+                    authorities = JsonPath.using(conf).parse(document).read("$..authorities");
+                }
+                Collection<String> roles = new ArrayList<>();
+                authorities.forEach(authorityItem -> {
+                    if (authorityItem instanceof String) {
+                        roles.add((String) authorityItem);
+                    } else if (authorityItem instanceof JSONArray) {
+                        roles.addAll((Collection<String>) authorityItem);
+                    } else if (authorityItem instanceof Collection) {
+                        roles.addAll((Collection<String>) authorityItem);
+                    }
+                });
+
+                for (String authority : roles) {
+                    grantedAuthorities.add(new SimpleGrantedAuthority(authority));
+                }
+                Map<String, Object> userAttributes = new HashMap<>(16);
+                userAttributes.put(userNameAttributeName, JsonPath.using(conf).parse(document).read("$." + userNameAttributeName));
+                userAttributes.put("preferred_username", JsonPath.using(conf).parse(document).read("$.preferred_username"));
+                userAttributes.put("email", JsonPath.using(conf).parse(document).read("$.email"));
+                OAuth2User oAuth2User = new DefaultOAuth2User(grantedAuthorities, userAttributes, userNameAttributeName);
+
+                return oAuth2User;
+            } catch (Exception e) {
+                log.error("oauth2UserService Exception", e);
+            }
+            return null;
         };
     }
 
